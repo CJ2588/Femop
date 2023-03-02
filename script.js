d2e6ac1327c573211fae2604850905d0ea177173
CJ2588 <chijioken15@gmail.com>
2023-03-02 18:16:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// code for products drop down menu
+
+const click = document.querySelectorAll(".click");
+const list = document.querySelectorAll(".dropdown-list");
+
+
+click[0].addEventListener("click",()=>{
+    list[0].classList.toggle("newlist");
+});
+
+click[1].addEventListener("click",()=>{
+    list[1].classList.toggle("newlist");
+});
+
+click[2].addEventListener("click",()=>{
+    list[2].classList.toggle("newlist");
+});
+
+click[3].addEventListener("click",()=>{
+    list[3].classList.toggle("newlist");
+});
+
+click[4].addEventListener("click",()=>{
+    list[4].classList.toggle("newlist");
+});
+
+
+
+
