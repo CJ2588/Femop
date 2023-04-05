3df1b7d4c2c60a98a4f46ab3505bc525f6f095c7
CJ2588 <chijioken15@gmail.com>
2023-04-05 16:28:51 +0100

    1.Added pics for product 2.changes to Js and css code

--- script.js
+++ script.js
@@ -4,26 +4,8 @@ const click = document.querySelectorAll(".click");
 const list = document.querySelectorAll(".dropdown-list");
 
 
-click[0].addEventListener("click",()=>{
-    list[0].classList.toggle("newlist");
-});
-
-click[1].addEventListener("click",()=>{
-    list[1].classList.toggle("newlist");
-});
-
-click[2].addEventListener("click",()=>{
-    list[2].classList.toggle("newlist");
-});
-
-click[3].addEventListener("click",()=>{
-    list[3].classList.toggle("newlist");
-});
-
-click[4].addEventListener("click",()=>{
-    list[4].classList.toggle("newlist");
-});
-
-
-
-
+for (let number = 0; number<=click.length-1; number++) {
+    click[number].addEventListener("click",()=>{
+        list[number].classList.toggle("newlist");
+    });
+}
